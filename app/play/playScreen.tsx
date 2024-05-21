286bc272b85cc3c9d23c504354fc6a5db195caa6
emil <emilio.haag@t-online.de>
2024-05-21 10:58:44 +0200

    grobe Home Page

---
+++ app/play/playScreen.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const playScreen = () => {
+    return (
+        <View>
+            <Text>Test2</Text>
+        </View>
+    );
+}
+
+export default playScreen;
