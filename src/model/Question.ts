bbe8c1a6f0ed6b6223db9bbd0b1c23b5fec58dae
Gillian Elfert <gillian.elfert@haw-hamburg.de>
2024-06-04 09:27:31 +0200

    Basic Quiz

---
+++ src/model/Question.ts
@@ -0,0 +1,11 @@
+export default class Question {
+    question: string;
+    answers: string[];
+    correctAnswer: string;
+  
+    constructor(question: string, answers: string[], correctAnswer: string) {
+      this.question = question;
+      this.answers = answers;
+      this.correctAnswer = correctAnswer;
+    }
+  }
